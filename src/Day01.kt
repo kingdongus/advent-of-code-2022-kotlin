571ff7da7afa77aa61fec4b80ddfef5e51a31069
sascha <chunkyboy.extrachunk@gmail.com>
2022-12-01 16:02:46 +0100

    Solutions for Day 1, part 1 and 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,39 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var partialSum = 0
+        var largest = 0
+
+        input.forEach {
+            if (it.isBlank()) partialSum = 0
+            else partialSum += it.toInt()
+            if (partialSum > largest) largest = partialSum
+        }
+        return largest
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var partialSum = 0
+        val partialSums = mutableListOf<Int>()
+
+        input.forEach {
+            if (it.isBlank()) {
+                partialSums += partialSum
+                partialSum = 0
+            }
+            else partialSum += it.toInt()
+        }
+        // dangling element
+        partialSums += partialSum
+
+        return partialSums.sorted().reversed().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("day01_test")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = readInput("day01_input")
     println(part1(input))
     println(part2(input))
 }
